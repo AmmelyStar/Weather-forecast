e5abb92c2f4cc9ec82dcdb4d322a47245f312daf
AmmelyStar <113164996+AmmelyStar@users.noreply.github.com>
2024-03-09 23:21:10 +0100

    edit switch

---
+++ src/switch.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector('.btn');
+
+function darkThemeSwitch() {
+     console.log("Dark theme switch function called"); 
+    const body = document.body;
+    const darkTheme = localStorage.getItem('darktheme') === 'true';
+
+
+    localStorage.setItem('darktheme', !darkTheme);
+    body.classList.toggle('dark-theme', !darkTheme);
+}
+
+
+btn.addEventListener('click', darkThemeSwitch);
+
+function onload() {
+    document.body.classList.toggle('dark-theme', localStorage.getItem('darktheme') === 'true')
+}
+document.addEventListener('DOMContentLoaded', onload)
